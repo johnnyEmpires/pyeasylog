74af32dcaaa0a41995d51d01149a3baec7830555
johnnyempires <imperialjohncarlo@gmail.com>
2020-10-09 21:12:00 +0900

    added setup

---
+++ easylog/__init__.py
@@ -0,0 +1,3 @@
+from easylog._logger import module_logger
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+from easylog
+setup(name='easylog', version=easylog.__version__, packages=find_packages())
