17cf8752d4de178782ae101e4a976c6bdb37e58b
johnnyempires <imperialjohncarlo@gmail.com>
2020-10-10 09:01:50 +0900

    setup update

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
 import pyeasylog
 
-setup(name=pyeasylog.__appname__, version=pyeasylog.__version__,
-    packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name=pyeasylog.__appname__,
+    version=pyeasylog.__version__,
+    packages=find_packages(),
+    author="john imperial",
+    description="a preconfigured logger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnnyEmpires/pyeasylog",
+    python_requires='>=3.6'
+    )
